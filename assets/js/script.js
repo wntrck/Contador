30390cd30e23c644ababa4979b27e05a20e09280
wntrck <brenowentrick@gmail.com>
2022-03-04 23:54:19 -0300

    Criação do projeto contador em JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function zerar() {
+    currentNumber = 0;
+    currentNumberWrapper.innerHTML = currentNumber
+}
